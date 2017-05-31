95036d5d61cd0cb1e6d691e409a75615124df79e
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-30 17:26:40 -0700

    Installed gulp-babel and add some ES6 code to test

--- chapter2_JavaScriptDevelopmentTools/es6/gulpfile.js
+++ chapter2_JavaScriptDevelopmentTools/es6/gulpfile.js
@@ -1,7 +1,15 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 
 // Gulp dependencies go here
 
 gulp.task('default', function () {
   // Gulp tasks go here
+  // Node source
+  gulp.src('es6/**/*.js').pipe(babel()).pipe(gulp.dest('dist'));
+
+  // browser source
+  gulp.src('public/es6/**/*.js')
+  .pipe(babel())
+  .pipe(gulp.dest('public/dist'));
 });
