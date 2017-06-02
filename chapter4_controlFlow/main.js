2c0d3cf5a75404893270956c2c3e1c76e03aed9c
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-06-02 11:08:47 -0700

    Fill collect winnings part inside the while loop

--- chapter4_controlFlow/main.js
+++ chapter4_controlFlow/main.js
@@ -31,6 +31,13 @@ while (funds > 1 && funds < 100) {
   }
 
   // collect winnings (if any)
+  let winnings = 0;
+  for (let die = 0; die < hand.length; die++) {
+    let face = hand[die];
+    if (bets[face] > 0) winnings = winnings + bets[face];
+  }
+
+  funds = funds + winnings;
 }
 
 // returns a random integer in the range [m, n] (inclusive)
