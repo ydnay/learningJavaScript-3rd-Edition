48c21ea96633fd6006516d377a87072205303121
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-06-02 11:13:24 -0700

    Move helper functions to the top of the file

--- chapter4_controlFlow/main.js
+++ chapter4_controlFlow/main.js
@@ -1,7 +1,21 @@
 // Crown and Anchor. 19th-century salilors betting game.
 
-let funds = 50; // starting conditions
+// helper functions:
 
+// returns a random integer in the range [m, n] (inclusive)
+function rand(m, n) {
+  return m + Math.floor((n - m + 1) * Math.random());
+}
+
+// randomly return a string representing one of the six
+// Crown and Anchor faces
+function randFace() {
+  return ['crown', 'anchor', 'heart', 'spade', 'club', 'diamond']
+    [rand(0, 5)];
+}
+
+// control flow
+let funds = 50; // starting conditions
 while (funds > 1 && funds < 100) {
   // place bets
   const bets = { crown: 0, anchor: 0, heart: 0,
@@ -38,16 +52,4 @@ while (funds > 1 && funds < 100) {
   }
 
   funds = funds + winnings;
-}
-
-// returns a random integer in the range [m, n] (inclusive)
-function rand(m, n) {
-  return m + Math.floor((n - m + 1) * Math.random());
-}
-
-// randomly return a string representing one of the six
-// Crown and Anchor faces
-function randFace() {
-  return ['crown', 'anchor', 'heart', 'spade', 'club', 'diamond']
-    [rand(0, 5)];
 }
